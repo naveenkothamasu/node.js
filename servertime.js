ee1276f3e5e98dd73455f5bcc084380d02d836e1
Naveen <kothamas@usc.edu>
2014-05-12 19:31:22 -0700

    working on 11

--- servertime.js
+++ servertime.js
@@ -1,11 +1,18 @@
 var net = require('net');
 var port = process.argv[2];
 
-net.createServer(function (socket){
-	//var date = new Date();
-	//"YYYY-MM-DD hh:mm"
-	//var data =date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()+"\n";
- 
-	socket.write("Hello");
-	socket.on('end',function(){console.log("Server disconnected")});
-}).listen(port);
+server = net.createServer(function (socket){
+		var date = new Date();
+		var month =  1+date.getMonth();
+		var sMonth = month;
+		if (month < 10){
+			sMonth = 0+""+month;
+		}
+		
+		//"YYYY-MM-DD hh:mm"
+		var data =date.getFullYear()+"-"+ sMonth +"-"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()+"\n";
+		socket.write(data+"\n");
+		socket.end();
+	});
+
+server.listen(port);
